ceed51649d7dad7b5c990e5b5b2f43cad72c08df
atimilson <atimilson95@gmail.com>
2020-07-15 20:22:32 -0400

    navegation

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './routes';
+
+const App = () => <Routes/>;
+
+export default App;
+
+/*
+class App extends Component{
+    render(){
+        return <Routes />
+    }
+} */
